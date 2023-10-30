629daa99b8e31528dba8af45ade3669374b222e9
meghagavankar1 <meghagavankar@nimapinfotech.com>
2023-10-30 17:27:30 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+		path: '', loadChildren: () => import('../app/core/core/core.module').then(m => m.CoreModule),
+		canActivate: []
+	},
+  {
+		path: 'view', loadChildren: () => import('../app/view/view.module').then(m => m.ViewModule),
+		canActivate: [],
+	},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/core/home/home.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { AngularMaterialModule } from 'src/app/angular-material/angular-material.module';
+
+const routes:Routes=[
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full'
+  }
+  ,
+  {
+    path:'home',
+    component:HomeComponent
+  }
+  
+  
+];
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    AngularMaterialModule
+  ]
+})
+export class HomeModule { }
